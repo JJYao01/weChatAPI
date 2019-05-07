dc07c13f4fbdda5af92d7b2ffb7d9a787dec2968
堯 <oncwnuJn4Qz5oL5i40DnCZbg1PJI@git.weixin.qq.com>
2019-05-07 17:34:52 +0800

    調整Payment Model欄位

--- weChatAPI/payment/models.py
+++ weChatAPI/payment/models.py
@@ -2,9 +2,10 @@ from django.db import models
 
 class Payment(models.Model):
     orderNo = models.IntegerField()
+    customerName = models.TextField(default=None, blank=True, null=True)
+    itemName = models.TextField(default=None, blank=True, null=True)
     payAmount = models.IntegerField()
-    paidAmount = models.IntegerField()
-    paydate = models.DateTimeField()
+    paydate = models.DateTimeField()    
     
     class Meta:
         db_table = 'payment'
