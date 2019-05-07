009653d5a83b9b3132a5dca97fe905e5c5a6801f
堯 <oncwnuJn4Qz5oL5i40DnCZbg1PJI@git.weixin.qq.com>
2019-05-07 19:43:35 +0800

    調整顯示畫面

--- weChatAPI/main/views.py
+++ weChatAPI/main/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render
+from payment.models import Payment
 
 
 def main(request):
     '''
     Show 'Hello world!' in the main page
     '''
-    context = {'like':'很棒'}
+    orders = Payment.objects.all()
+    context = {'orders':orders}
     return render(request, 'main/main.html', context)
