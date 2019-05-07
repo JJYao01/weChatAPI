4be7d4ad2d55d3b51498a600a9f3ed2b94cbbf5f
堯 <oncwnuJn4Qz5oL5i40DnCZbg1PJI@git.weixin.qq.com>
2019-05-07 20:51:34 +0800

    bug fix

--- weChatAPI/main/views.py
+++ weChatAPI/main/views.py
@@ -6,6 +6,6 @@ def main(request):
     '''
     Show 'Hello world!' in the main page
     '''
-    orders = Payment.objects.all().exclude(customerName=None).order_by('_id')
+    orders = Payment.objects.all().exclude(customerName=None).order_by('id')
     context = {'orders':orders}
     return render(request, 'main/main.html', context)
